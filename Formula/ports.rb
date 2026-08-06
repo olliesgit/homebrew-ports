1c92840ee47dc458d3d324ed5fcc15ff356b9417
olliesgit <40745737+olliesgit@users.noreply.github.com>
2026-08-07 07:03:13 +1000

    Bump to v1.0.1

--- Formula/ports.rb
+++ Formula/ports.rb
@@ -1,8 +1,8 @@
 class Ports < Formula
   desc "CLI Ports Table - what's running on your macOS ports"
   homepage "https://github.com/olliesgit/cli-ports-table"
-  url "https://github.com/olliesgit/cli-ports-table/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "afdabd03f5d38d1d5c7765b90151f80149fd1539fc923d0789ed93ce770ec99c"
+  url "https://github.com/olliesgit/cli-ports-table/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "2d9a53a5ab4d2b7afb1297eb3ad64a0469c6b2b88a2134ff19e38010c1a0f09d"
   license "MIT"
 
   def install
